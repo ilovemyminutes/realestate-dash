80a3969d3fb533728bab84efd900f9970c5f9697
ilovemyminutes <silkstaff@naver.com>
2025-12-08 03:14:17 +0900

    feat: use streamlit secrets for GCP credentials

--- utils/bq_client.py
+++ utils/bq_client.py
@@ -3,14 +3,28 @@ import os
 
 import streamlit as st
 from google.cloud import bigquery
+from google.oauth2 import service_account
 
 
 @st.cache_resource
 def get_bq_client():
-    """BigQuery 클라이언트 생성 (캐시됨)"""
+    """BigQuery 클라이언트 생성 (캐시됨)
+
+    - Streamlit Cloud: st.secrets에서 credential 로드
+    - 로컬 개발: credential 파일 또는 .streamlit/secrets.toml 사용
+    """
+    # Streamlit Secrets 사용 (Cloud 배포용)
+    if "gcp_service_account" in st.secrets:
+        credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
+        return bigquery.Client(credentials=credentials, project=credentials.project_id)
+
+    # 로컬 개발용: credential 파일 사용
     cred_path = "credential/ilovemyrealestate-27f37b5ebb2a.json"
     if os.path.exists(cred_path):
         os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = cred_path
+        return bigquery.Client()
+
+    # 기본 (ADC - Application Default Credentials)
     return bigquery.Client()
 
 
